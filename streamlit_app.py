1f9ccc45d320c25e4079dd78e366f9caf950e0db
hisnameisdev <140297057+hisnameisdev@users.noreply.github.com>
2026-01-20 10:32:11 +0800

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("My Technical Portfolio")
+st.write("Current Status: Building resilience and shipping code.")
+st.write("---")
+st.write("Hello, World! This site is live.")
